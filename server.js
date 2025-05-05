608bfb9de440274dba2b1931ea8eb9fadaba4803
Kwanerek <161065818+Kwanerek@users.noreply.github.com>
2025-05-05 16:27:12 +0200

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
-
+http.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serwer działa na porcie ${PORT}`);
+  
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
