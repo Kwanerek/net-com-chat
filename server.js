b8ec127f66bd922ef230c4d0069730389382dcf8
Kwanerek <161065818+Kwanerek@users.noreply.github.com>
2025-05-06 15:57:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-// Serwuj pliki statyczne (HTML, CSS, JS) z bieżącego folderu
-app.use(express.static(__dirname));
+let messages = [];
 
-// Główna strona (index.html)
+app.use(express.static(path.join(__dirname, 'public'))); // zakładamy, że index.html i style.css są w folderze 'public'
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Obsługa Socket.IO
 io.on('connection', (socket) => {
   console.log('Użytkownik połączony');
 
   socket.on('chat message', (msg) => {
+    messages.push(msg);
     io.emit('chat message', msg);
   });
 
+  socket.on('delete message', (id) => {
+    messages = messages.filter(msg => msg.id !== id);
+    io.emit('delete message', id);
+  });
+
   socket.on('disconnect', () => {
-    console.log('Użytkownik się rozłączył');
+    console.log('Użytkownik rozłączony');
   });
 });
 
-// Port Rendera lub domyślnie 3000
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
 });
