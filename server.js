87edb4558ad7755edb11a83c4f38bd01fe59458f
Kwanerek <161065818+Kwanerek@users.noreply.github.com>
2025-05-06 16:51:34 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('Użytkownik połączony');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('delete message', (id) => {
+    io.emit('delete message', id);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Użytkownik rozłączony');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Serwer działa na porcie ${PORT}`);
+});
