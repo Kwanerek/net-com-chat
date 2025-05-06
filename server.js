4a9ad076028e9b041432feea0234690553dfe249
Kwanerek <161065818+Kwanerek@users.noreply.github.com>
2025-05-06 20:31:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'main')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'main', 'index.html'));
 });
 
 io.on('connection', (socket) => {
